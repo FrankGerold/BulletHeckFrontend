cddb5ef11379b6fb0cd998a9e7529bdc233c4dc3
Frank Gerold <FrankETSGerold@gmail.com>
2020-01-02 11:50:00 -0500

    new color scheme and alignment tweaks

--- src/canvas.js
+++ src/canvas.js
@@ -7,23 +7,23 @@ const Game = (x) => {
     x.drawingContext.shadowOffsetX = 5;
     x.drawingContext.shadowOffsetY = 5;
     // x.drawingContext.shadowBlur = 10;
-    x.drawingContext.shadowColor = "pink";
+    x.drawingContext.shadowColor = "#F3F7F4";
     // x.background(200);
+    x.frameRate(60);
   }
 
   x.draw = () => {
-    x.background('#FF7083');
+    x.background('#BA3B52');
     let score = x.text(Math.round(x.frameCount/30), 100, 100)
-    // x.background(0);
-    // x.fill(255);
+    x.fill('#FF7083');
     let oval = x.ellipse(x.mouseX, x.mouseY, 80, 50);
 
     let rectangle = x.rect(0, 0, 50, 30);
     if ((x.keyIsPressed == true) && (x.key == 'ArrowLeft')) {
-      rectangle.x -= 7;
+      rectangle.x -= 5;
     }
     if ((x.keyIsPressed == true) && (x.key == 'ArrowRight')) {
-      rectangle.x += 7;
+      rectangle.x += 5;
     }
   }
 }
